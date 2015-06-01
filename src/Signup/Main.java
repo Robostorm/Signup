fe390e4f5b4f65316b7d3d692365432f67ac2413
zethra <jediben97@gmail.com>
2015-05-31 20:57:07 -0400

    First commit

---
+++ src/Signup/Main.java
@@ -0,0 +1,25 @@
+package Signup;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Robostrom Signup");
+        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
